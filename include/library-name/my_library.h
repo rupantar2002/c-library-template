38acddf8c4d31f3d87c262c8e9fa8295f694dfa0
rupantar2002 <rupantaradhikary.2002@gmail.com>
2024-05-27 14:58:31 +0530

    added src and header files for my library

---
+++ include/library-name/my_library.h
@@ -0,0 +1,8 @@
+#ifndef __MY_LIBRARY_H__
+#define __MY_LIBRARY_H__
+
+int my_library_add(int val1, int val2);
+
+int my_library_subtract(int val1, int val2);
+
+#endif //__MY_LIBRARY_H__
---
+++ src/my_library.c
@@ -0,0 +1,11 @@
+#include "my_library.h"
+
+int my_library_add(int val1, int val2)
+{
+    return val1 + val2;
+}
+
+int my_library_subtract(int val1, int val2)
+{
+    return val1 - val2;
+}

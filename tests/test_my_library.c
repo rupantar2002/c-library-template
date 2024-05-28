b13596822ad7c5a6aedb2fac82189fcad187c16e
rupantar2002 <rupantaradhikary.2002@gmail.com>
2024-05-28 16:54:31 +0530

    unity testing framework working

--- tests/test_my_library.c
+++ tests/test_my_library.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
-#include <my_library.h>
+#include "unity.h"
+#include "my_library.h"
 
-int main(int argc, char const *argv[])
+void setUp(void)
 {
-    printf("The sum of 12 and 13 is : %d \r\n",my_library_add(12,13));
-    printf("The difference of 12 and 13 is : %d \r\n",my_library_subtract(12,13));
-    return 0;
+    // Set up before each test
+    printf("%s : called\r\n", __func__);
+}
+
+void tearDown(void)
+{
+    // Clean up after each test
+    printf("%s : called\r\n", __func__);
+}
+
+void test_function1(void)
+{
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(3, my_library_add(1, 15), "!Error in my_library_add(1,2)");
+    // TEST_ASSERT_EQUAL(3, my_library_add(1,2));
+}
+
+void test_function2(void)
+{
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(-1, my_library_subtract(1, 2), "!Error in my_library_subtract(1,2)");
+    // TEST_ASSERT_EQUAL(3, my_library_add(1,2));
+}
+
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_function1);
+    RUN_TEST(test_function2);
+    return UNITY_END();
 }
